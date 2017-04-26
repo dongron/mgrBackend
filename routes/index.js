82148f02cba1aa20363f3d325819847c67134c59
Dominik <dongron@wp.pl>
2017-04-26 11:30:17 +0200

    Login form added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
+  res.render('./login/login', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
