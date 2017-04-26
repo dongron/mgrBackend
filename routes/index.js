60065d5cd02f73a038310b06b319aff56bbe8501
Dominik <dongron@wp.pl>
2017-04-26 11:47:10 +0200

    Some views added

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,34 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('./login/login', { title: 'Express' });
+    res.render('./login/login', { title: 'Express' });
+});
+
+router.post('/login', function (req, res, next) {
+    let email = req.body.email;
+    let password = req.body.password;
+    
+    User.findOne({'email': email}).then(
+        user => {
+            if(!user) {                
+                res.send("Not valid email! Reload page");
+            } else {
+                if(user.email == email && user.password == password) {
+                    
+                } else {                    
+                    res.send("Not valid email or password! Reload page");
+                }
+            }
+        },
+        err => {
+            console.error(err);
+        }
+    );
 });
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
