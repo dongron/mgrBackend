2b47ba3d2b19e0770c7f9c6e3276f0c99edceb28
Dominik <dongron@wp.pl>
2017-04-30 19:10:24 +0200

    Reservation model added

---
+++ models/ReservationModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ReservationSchema  = Schema({
+    clientEmail: {type: String, required: true},
+    ownerEmail: {type: String, required: true},
+    time: {type: Date},
+    long: {type: Number}    //hours 
+});
+
+module.exports = mongoose.model('Reservation', ReservationSchema, reservations);
