f27197485285d46c8cf3565115f95988eb31f520
Dominik <dongron@wp.pl>
2017-04-30 13:16:03 +0200

    Added test places view

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const User = require("../models/UserModel");
 
 
 router.get('/', function(req, res, next) {
-    res.render('./login/login', { title: 'Express' });
+    let validText;
+    if(req.query.valid) {
+        validText = 'Invalid email or password';
+    }
+    res.render('./login/login', { title: '', validText: validText});
 });
 
 router.post('/login', function (req, res, next) {
     let email = req.body.email;
-    let password = req.body.password;
+    let password = req.body.password; 
     
     User.findOne({'email': email}).then(
         user => {
-            if(!user) {                
-                res.send("Not valid email! Reload page");
+            if(!user) {
+                res.redirect('/?valid=notValid');
             } else {
                 if(user.email == email && user.password == password) {
-                    
+                    console.log("VALID AND PASS");
+                    res.redirect('/home');                    
                 } else {                    
                     res.send("Not valid email or password! Reload page");
                 }
@@ -30,7 +36,22 @@ router.post('/login', function (req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    var places = [
+        {
+            name: "nameTest",
+            street: "streetTest",
+            streetNr: "testNr99",
+            localNr: "localNrTest00"
+        },
+        {
+            name: "2nameTest",
+            street: "2streetTest",
+            streetNr: "2testNr99",
+            localNr: "2localNrTest00"
+        }
+];
+    
+    res.render('./places/placesHome', { userCity: 'Express', places: places });
 });
 
 module.exports = router;
