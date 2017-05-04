b2757d8ec13fb615788015d587e1532829674f62
Dominik <dongron@wp.pl>
2017-05-04 10:02:07 +0200

    Bug with same second port opening i app.js fixed

--- app.js
+++ app.js
@@ -75,8 +75,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var listener = app.listen(3000, function(){
-    console.log('Listening on port ' + listener.address().port)
-});
-
 module.exports = app;
