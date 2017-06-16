25f0e4b6dfebba278568e4396f0e2cdf9bf307ad
Dominik <dongron@wp.pl>
2017-06-17 01:15:26 +0200

    Get place by id added in api

--- routes/api/places.js
+++ routes/api/places.js
@@ -16,13 +16,13 @@ router.get('/all', function(req, res, next) {
 	Place.find({}).then(
         list => {
             if(list.length == 0) {
-                res.send('no places'); 
+                res.send('no places');
             } else {
                 var placeMap = {};
                 list.forEach(function(place) {
                     placeMap[place._id] = place;
                 });
-                res.send(placeMap);  
+                res.send(placeMap);
             }
         },
         err => console.error(err)
@@ -34,28 +34,45 @@ router.post('/add', function(req, res) {
 		res.send("ERROR, name and geo are minimal data!");
 		return;
 	}
-	
+
 	var placeToSave = {
 		name: req.body.name ||"none",
 		street: req.body.street || "none",
-		streetNr: req.body.streetNr, 
-		localNr: req.body.localNr, 
+		streetNr: req.body.streetNr,
+		localNr: req.body.localNr,
 		city: req.body.city,
 		geo: req.body.geo || "none",
 		ownerEmail: req.body.ownerEmail
-	};	
+	};
 	var newPlace = new Place(placeToSave);
 	newPlace.save();
 	res.send("OK");
 });
 
+router.get('/byId/:id', function(req, res, next) {
+    let id = req.params.id;
+
+    Place.findOne({'_id': id}).then(
+        place => {
+            if(!place) {
+                res.send("place not found");
+            } else {
+                res.send(place);
+            }
+        },
+        err => {
+            console.error(err);
+        }
+    );
+});
+
 router.get('/byName/:name', function(req, res, next) {
     let name = req.params.name;
 	console.log(name);
-    
+
     Place.findOne({'name': name}).then(
         place => {
-            if(!place) {                
+            if(!place) {
                 res.send("place not found");
             } else {
                 res.send(place);
@@ -69,10 +86,10 @@ router.get('/byName/:name', function(req, res, next) {
 
 router.get('/query', function(req, res, next) {
     let city = req.query.city;
-    
+
     Place.findOne({'city': city}).then(
         place => {
-            if(!place) {                
+            if(!place) {
                 res.send("place not found");
             } else {
                 res.send(place);
