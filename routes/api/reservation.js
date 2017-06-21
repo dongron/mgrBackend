5de7b9f15ea7bd38edbb00fda820ce4ec909d8ee
Dominik <dongron@wp.pl>
2017-06-22 00:07:51 +0200

    Updating reservation done

--- routes/api/reservation.js
+++ routes/api/reservation.js
@@ -62,7 +62,58 @@ router.delete('/remove', function (req, res) {
     );
 });
 
-router.put('/update', function(req, res) {
+router.put('/update', function (req, res) {
+    if(!req.body.clientEmail || !req.body.time || !req.body.placeName) {
+        console.log('reservation not added', req.body);
+        res.send("ERROR, clientEmail, ownerEmail and reservation data needed!");
+        return;
+    }
+
+    const objectChanges = {
+        $set: {
+            clientEmail: req.body.clientEmail,
+            time: req.body.time,
+            long: req.body.long ? req.body.long : long
+        },
+        $inc: { __v: 1 }
+    };
+
+    Reservation.update({ _id: req.body._id }, objectChanges, { multi: true }).then(
+        (doc) => {
+                console.log('updated', doc);
+                res.send("OK");
+            },
+            (err) => {
+                console.error(err);
+                res.send(err);
+            });
+
+// alternate, not sure if it worked
+    // Reservation.findOne({ _id: req.body._id }).then(
+    //     (doc) => {
+    //         console.log('found', doc);
+    //         doc.clientEmail = req.body.clientEmail? req.body.clientEmail : doc.clientEmail;
+    //         doc.ownerEmail = doc.ownerEmail;
+    //         doc.placeName = doc.placeName;
+    //         doc.time = req.body.time? req.body.time : doc.time;
+    //         doc.long = req.body.long? req.body.long : doc.long;
+    //         if(doc.clientEmail != req.body.clientEmail || doc.time != req.body.time ||  doc.long != req.body.long) {
+    //             console.log('+++ CHANGES +++');
+    //             doc.__v = doc.__v++;
+    //         }
+    //         console.log('changed',doc);
+    //         doc.save();
+    //         res.send("OK");
+    //     },
+    //     (err) => {
+    //         console.error(err);
+    //         res.send(err);
+    //     });
+
+
+});
+
+router.put('/updateFake', function(req, res) {
     console.log('--reservation post request body', req.body);
     if(!req.body.clientEmail || !req.body.time || !req.body.placeName) {
         console.log('reservation not added', req.body);
@@ -89,8 +140,6 @@ router.put('/update', function(req, res) {
     },
         (err) => console.error(err)
     );
-
-
 });
 
 module.exports = router;
